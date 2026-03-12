39b7de62fe4c6b02382296741d5954c22d52b911
GrucaCode <basiag2001@gmail.com>
2026-03-12 13:24:34 +0100

    refactor hideSection feature

--- public/js/hideSection.js
+++ public/js/hideSection.js
@@ -4,13 +4,11 @@ const dataText = document.querySelector(".data-drop-up-text");
 const dataIcon = document.querySelector(".data-drop-up-icon");
 
 const toggleSection = (e) => {
-    dataSec.classList.toggle('hide');
-    if (dataSec.classList.contains('hide')) {
-        dataSec.style.display ="none";
+    dataSec.classList.toggle('hidden');
+    if (dataSec.classList.contains('hidden')) {
         dataText.textContent = "rozwiń sekcję";
         dataIcon.textContent = "arrow_drop_down";
     } else {
-        dataSec.style.display ="block";
         dataText.textContent = "zwiń sekcję";
         dataIcon.textContent = "arrow_drop_up";
     }
