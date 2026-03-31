7226eaee54a4b812e13d93ee6b1fbc5b7db8479c
GrucaCode <basiag2001@gmail.com>
2026-03-31 14:49:10 +0200

    refactor text search feature
    - divide code into smaller functions
    - remove localStorage
    - add event delegation

--- public/js/search.js
+++ public/js/search.js
@@ -1,127 +1,116 @@
+import { toPLDate } from "./utils.js";
+import { goToArticle } from "./utils.js";
+
 document.addEventListener('DOMContentLoaded', () => {
-  const input = document.getElementById('searchQuery');
+  const searchInput = document.getElementById('searchQuery');
   const searchBtn = document.querySelector('.search-btn');
   const micBtn = document.querySelector('.mic-btn');
   const micIcon = document.getElementById('mic-icon');
   const clearBtn = document.querySelector('.clean-btn');
-  const resultSec = document.querySelector('.result-sec');
+  const resultsWrap = document.querySelector('.results');
 
-  // Funkcja wyświetlająca wyniki wyszukiwania
-  function renderResults(news = []) {
-    const old = resultSec.querySelector('.results');
-    if (old) old.remove();
+  const hideResults = () => {
+    resultsWrap.innerHTML = '';
+    resultsWrap.setAttribute('hidden', true);
+  }
 
-    const wrap = document.createElement('div');
-    wrap.className = 'results';
-    if (!news.length) {
-      wrap.innerHTML = `<p>Nie znaleziono wyników dla podanego zapytania. Użyj innego słowa lub frazy</p><img src="img/Empty_graphics.svg" alt="Grafika informująca o braku wyników">`;
-      resultSec.appendChild(wrap);
+  const prepareToRender = () => {
+    resultsWrap.innerHTML = '';
+    resultsWrap.removeAttribute('hidden');
+  }
+
+  const clearResults = (e) => {
+    e.preventDefault();
+    searchInput.value = '';
+    hideResults();
+  }
+
+  const renderEmptyResults = () => {
+    resultsWrap.innerHTML = `
+      <p>Nie znaleziono wyników dla podanego zapytania. Użyj innego słowa lub frazy</p>
+      <img src="img/Empty_graphics.svg" alt="Grafika informująca o braku wyników">
+    `;
+  }
+
+  const renderResultCard = (article) => {
+      const date = toPLDate(article.publish_date);
+
+      resultsWrap.insertAdjacentHTML('beforeend',`
+        <article class="result-card">
+          ${article.image ? `<img src="${article.image}" alt="${article.title}" class="result-card__img">`: ""}
+          <div class="result-card__content">
+            <h3 class="result-card__title">${article.title}</h3>
+              <div class="result-card__actions">
+                <button class="btn-read-more" data-article-id="${article.id}">
+                  <div class="btn-read-more__frame">Czytaj</div>
+                </button>
+              </div>
+            <img src="img/Menu line.svg" alt="linia dekoracyjna oddzielająca menu od tekstu" class="result-card__decor-line">
+            <div class="result-card__info">
+              <p class="result-card__label">Data:</p>
+              <p class="result-card__date">${date}</p>
+            </div>
+          </div>
+        </article>
+      `);
+  }
+
+  const renderResults = (foundNews = []) => {
+    prepareToRender();
+
+    if(!foundNews.length) {
+      renderEmptyResults();
       return;
     }
 
-    // Wyświetlenie wyników wyszukiwania
-    news.forEach((article, idx) => {
-      const card = document.createElement('article');
-      card.className = 'result-card';
-      const img = article.image ? `<img class="result-card__img" src="${article.image}" alt="${article.title}">` : '';
-      const date = article.publish_date ? new Date(article.publish_date).toLocaleDateString('pl-PL') : '';
-      const summary = article.summary;
-      const artText =  article.text || '';
-
-      const tmpId = `${Date.now()}-${idx}`;
-      const goLocalBtn = `
-        <button class="btn-read-more" data-article-id="${tmpId}">
-          <div class="btn-read-more__frame">Czytaj</div>
-        </button>
-      `;
-
-      card.innerHTML = `
-        ${img}
-        <div class="result-card__content">
-          <h3 class="result-card__title">${article.title}</h3>
-            <div class="result-card__actions">
-            ${goLocalBtn}
-            </div>
-          <img src="img/Menu line.svg" alt="linia dekoracyjna oddzielająca menu od tekstu" class="result-card__decor-line">
-          <div class="result-card__info">
-            <p class="result-card__label">Data:</p>
-            <p class="result-card__date">${date}</p>
-          </div>
-        </div>
-      `;
-
-      // Zapisywnaie danych do localStorage do odczytu article.html
-      const payload = {
-        id: tmpId,
-        title: article.title,
-        author: article.author,
-        image: article.image,
-        url: article.url,
-        text: article.text || '',
-        summary: article.summary,
-        publish_date: article.publish_date,
-      };
-      localStorage.setItem(`article-${tmpId}`, JSON.stringify(payload));
-      wrap.appendChild(card);
-    });
-
-    resultSec.appendChild(wrap);
-    wrap.querySelectorAll('.btn-read-more').forEach(btn => {
-      btn.addEventListener('click', (e) => {
-        const id = e.currentTarget.getAttribute('data-article-id');
-        window.location.href = `article.html?id=${encodeURIComponent(id)}`;
-      });
+    foundNews.forEach((article) => {
+      renderResultCard(article);
     });
   }
 
-  // Szukanie
-  async function performSearch() {
-    const q = input.value.trim();
+  const performSearch = async (e) => {
+    e.preventDefault();
+    const q = searchInput.value.trim();
     if (!q) {
-      renderResultsAfterClear();
+      hideResults();
       return;
     }
     try {
-      renderResults([]); 
       const r = await fetch(`/api/news/search?q=${encodeURIComponent(q)}`);
       const data = await r.json();
+
       if (!data.success) {
         renderResults([]);
         return;
       }
-      // Wyszukiwania są zwracane z api w formie tablicy data.news
+
       renderResults(data.news || []);
-    } catch (e) {
-      console.error(e);
+    } catch (err) {
+      console.error(err);
       renderResults([]);
     }
   }
 
-  function renderResultsAfterClear(news = []) {
-    const old = resultSec.querySelector('.results');
-    if (old) old.remove();
-    }
+  resultsWrap.addEventListener('click', (e) => {
+    const btn = e.target.closest('.btn-read-more');
+    if(!btn) return;
 
-  searchBtn?.addEventListener('click', (e) => {
-    e.preventDefault();
-    performSearch();
+    const newsId = btn.getAttribute('data-article-id');
+    goToArticle({id: newsId});
+  });  
+
+  searchBtn?.addEventListener('click', (e) => performSearch(e));
+
+  searchInput?.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') performSearch(e);
   });
 
-  input?.addEventListener('keydown', (e) => {
-    if (e.key === 'Enter') {
-      e.preventDefault();
-      performSearch();
-    }
-  });
+  clearBtn?.addEventListener('click', (e) => clearResults(e));
 
-  clearBtn?.addEventListener('click', (e) => {
-    e.preventDefault();
-    input.value = '';
-    renderResultsAfterClear([]);
-  });
-
+  
   // Wyszukiwanie głosowe
   const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
   if (micBtn && SpeechRecognition) {
     const recog = new SpeechRecognition();
     recog.lang = 'pl-PL';
@@ -147,7 +136,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     recog.onresult = (event) => {
       const transcript = event.results[0][0].transcript;
-      input.value = transcript;
+      searchInput.value = transcript;
       micIcon.textContent = 'mic';
       listening = false;
       performSearch();
@@ -162,6 +151,7 @@ document.addEventListener('DOMContentLoaded', () => {
       micIcon.textContent = 'mic';
       listening = false;
     };
+
   } else if (micBtn) {
     micBtn.addEventListener('click', (e) => {
       e.preventDefault();
